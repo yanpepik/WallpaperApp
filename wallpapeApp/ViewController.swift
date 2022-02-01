5bce634d0ec050c170640ab4b106e51fc3ac4f73
yanpepik <56980028+yanpepik@users.noreply.github.com>
2022-02-01 12:43:36 +0300

    Initial Commit

---
+++ wallpapeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wallpapeApp
+//
+//  Created by Yan Pepik on 1.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
